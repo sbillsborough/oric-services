e2586eb336ba1843a6673fb397de9a3fa6448a7b
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-08-21 18:17:28 +0100

    added a footer

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,16 @@
+// STICKY NAVBAR
 
+// Pull in the .nav element and add event listener
+
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
+
+// Function to compare navbar position on the page and set an active status once a condition is met
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
