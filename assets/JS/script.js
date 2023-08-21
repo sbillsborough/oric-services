15b8a3a9cb5021023e81ec1bbcea47ab003abd0a
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-08-21 18:26:23 +0100

    removed the merge conflict notation

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+HEAD;
 // STICKY NAVBAR
 
 // Pull in the .nav element and add event listener
@@ -8,11 +8,10 @@ window.addEventListener("scroll", fixNav);
 
 // Function to compare navbar position on the page and set an active status once a condition is met
 
-=======
 const nav = document.querySelector(".nav");
 window.addEventListener("scroll", fixNav);
 
->>>>>>> sticky-nav-bar
+sticky - nav - bar;
 function fixNav() {
   if (window.scrollY > nav.offsetHeight + 150) {
     nav.classList.add("active");
