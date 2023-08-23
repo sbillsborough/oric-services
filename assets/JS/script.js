5cdb6ae1726f27cea2065d6a9383d2753a9817ec
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-08-23 16:23:24 +0100

    stored button elements into button variable with getElementsByClassName

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,6 +5,10 @@
 const nav = document.querySelector(".nav");
 window.addEventListener("scroll", fixNav);
 
+// get all a tags with the btn class
+
+const buttons = nav.getElementsByClassName("btn");
+
 // Function to compare navbar position on the page and set an active status once a condition is met
 
 function fixNav() {
